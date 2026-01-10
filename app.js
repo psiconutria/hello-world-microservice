6c9fa8daa1fad8082318f791af7b0e9a28fd43d7
magdaleno <magdaleno.garcia@fraternitas.io>
2026-01-09 20:30:31 -0600

    test case 1

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("hello world");
+  res.send("hello world, Test case 1");
 });
 
 app.listen(3000, () => {
